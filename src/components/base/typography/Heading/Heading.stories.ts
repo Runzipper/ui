81b5084d36391c1ec1eff7184a5bd8aa8d71f3e6
BHyeonKim <rlaqhguse@gmail.com>
2025-10-27 23:13:44 +0900

    docs(typography): Add storybook stories

---
+++ src/components/base/typography/Heading/Heading.stories.ts
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import type { ReactElement, ReactNode } from 'react';
+import type { Text } from 'types';
+import Heading from './index';
+
+type HeadingProps = {
+	textType: Text;
+	children: ReactNode;
+};
+
+const meta = {
+	component: Heading as (props: HeadingProps) => ReactElement,
+	parameters: {
+		layout: 'centered',
+	},
+	argTypes: {
+		textType: {
+			control: 'select',
+			options: [
+				'h1',
+				'h2',
+				'h3',
+				'h4',
+				'h5',
+				'h6',
+				'p',
+				'span',
+				'strong',
+				'em',
+			],
+		},
+	},
+} satisfies Meta<HeadingProps>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+	args: {
+		textType: 'h1',
+		children: 'This is primary heading.',
+	},
+};
+
+export const Secondary: Story = {
+	args: {
+		textType: 'h2',
+		children: 'This is secondary heading.',
+	},
+};
