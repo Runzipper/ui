53fee797e24ad6088a08e8b9cedc376e786bd062
BHyeonKim <rlaqhguse@gmail.com>
2025-10-29 20:58:12 +0900

    feat: Add icon assets

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,6 @@
+export { ReactComponent as IconCheck } from './check.svg';
+export { ReactComponent as IconCloud } from './cloud.svg';
+export { ReactComponent as IconExclamation } from './exclamation.svg';
+export { ReactComponent as IconFile } from './file.svg';
+export { ReactComponent as IconUpload } from './upload.svg';
+export { ReactComponent as IconX } from './x.svg';
