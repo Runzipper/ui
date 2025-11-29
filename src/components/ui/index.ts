b6b60d4bec360c9039dc56acca0d0bf6db97d205
BHyeonKim <rlaqhguse@gmail.com>
2025-11-29 19:42:46 +0900

    feat: Export UI as namespace object

---
+++ src/components/ui/index.ts
@@ -0,0 +1,13 @@
+import Button from './Button';
+import FileDrop from './FileDrop';
+import Notification from './Notification';
+import Table from './Table';
+
+const UI = {
+	FileDrop,
+	Button,
+	Notification,
+	Table,
+};
+
+export default UI;
