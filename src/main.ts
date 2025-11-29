e86451566fbb7c66fdfb8d91c2c3033291084539
BHyeonKim <rlaqhguse@gmail.com>
2025-11-29 22:56:39 +0900

    feat: Add theme to entry file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Icon from 'components/base/icon';
 import Typography from 'components/base/typography';
 import UI from 'components/ui';
+import { vars } from 'styles/theme.css';
 
-export { Icon, Typography, UI };
+export { Icon, vars as Theme, Typography, UI };
