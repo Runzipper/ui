369f2bee3afacd631325bfc8e0b44fcc3908962f
BHyeonKim <rlaqhguse@gmail.com>
2025-12-01 01:16:11 +0900

    fix(build): Use relative imports for proper reference

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-export { default as Icon } from 'components/base/icon';
-export { default as Typography } from 'components/base/typography';
-export { default as UI } from 'components/ui';
-export { vars as Theme } from 'styles/theme.css';
+export { default as Icon } from './components/base/icon';
+export { default as Typography } from './components/base/typography';
+export { default as UI } from './components/ui';
+export { vars as Theme } from './styles/theme.css';
+
