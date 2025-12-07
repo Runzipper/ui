4f7b043266492bc65270f26124dbdd1d08078c5c
BHyeonKim <rlaqhguse@gmail.com>
2025-12-08 01:12:05 +0900

    style(button): Change button font family

--- src/components/ui/Button/button.css.ts
+++ src/components/ui/Button/button.css.ts
@@ -2,6 +2,7 @@ import { style } from '@vanilla-extract/css';
 import { vars } from 'styles/theme.css';
 
 export const buttonStyle = style({
+	fontFamily: vars.font.pretendard,
 	backgroundColor: vars.color.primary,
 	color: vars.color.white,
 	borderRadius: vars.radius.lg,
