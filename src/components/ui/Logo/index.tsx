9973cb77980e70c318ad5aa28d25a7d3e3610660
BHyeonKim <rlaqhguse@gmail.com>
2025-12-08 15:16:31 +0900

    fix: Fix circular import error

--- src/components/ui/Logo/index.tsx
+++ src/components/ui/Logo/index.tsx
@@ -1,12 +1,12 @@
-import { Icon } from 'main';
 import type { ComponentProps } from 'react';
+import Icon from '../../base/icon';
 import { logoVarient } from './logo.css';
 
 type LogoProps = {
-  type?: keyof typeof logoVarient
-} & ComponentProps<'div'>
+	type?: keyof typeof logoVarient;
+} & ComponentProps<'div'>;
 
-const Logo = ({type = 'primary', ...props}:LogoProps) => {
+const Logo = ({ type = 'primary', ...props }: LogoProps) => {
 	return (
 		<div className={logoVarient[type]} {...props}>
 			<Icon icon="IconFile" />
